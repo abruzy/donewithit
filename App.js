92660cc2089c0f07871acf403b4273cdc86ce5aa
Abuabakar Diallo <dialloabubakar5861@gmail.com>
2020-12-10 16:18:47 +0100

    feat: implementation of screens

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+
+export default function App() {
+  return <ListingEditScreen />;
+}
